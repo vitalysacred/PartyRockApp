cbe66869a481c2a3807df470f2429e2453be0153
vitalysacred <vitalysacred@gmail.com>
2016-09-22 01:59:31 +0700

    add a second screen with webform

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Vitaly Sacred on 22/09/16.
+//  Copyright © 2016 Vitaly Sacred. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
