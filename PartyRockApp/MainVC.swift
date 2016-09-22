cd01ff16dca3b8b865acd282d90d3258a7d51ec8
vitalysacred <vitalysacred@gmail.com>
2016-09-23 00:35:58 +0700

    add new 5 rows to the tableView / fix the problem with delegate dates

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -16,7 +16,51 @@ class MainVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        let p1 = partyRock(imageURL: "https://d13yacurqjgara.cloudfront.net/users/413563/screenshots/2979485/seasonofwonder_texture.png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cDBYKRjYZho\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "I love uor voice")
+        
+        partyRocks.append(p1)
+        
+        let p2 = partyRock(imageURL: "https://d13yacurqjgara.cloudfront.net/users/413563/screenshots/2979485/seasonofwonder_texture.png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cDBYKRjYZho\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "I love uor voice")
+        
+        partyRocks.append(p2)
+        
+        let p3 = partyRock(imageURL: "https://d13yacurqjgara.cloudfront.net/users/413563/screenshots/2979485/seasonofwonder_texture.png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cDBYKRjYZho\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "I love uor voice")
+        
+        partyRocks.append(p3)
+        
+        
+        let p4 = partyRock(imageURL: "https://d13yacurqjgara.cloudfront.net/users/413563/screenshots/2979485/seasonofwonder_texture.png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cDBYKRjYZho\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "I love uor voice")
+        
+        partyRocks.append(p4)
+        
+        let p5 = partyRock(imageURL: "https://d13yacurqjgara.cloudfront.net/users/413563/screenshots/2979485/seasonofwonder_texture.png", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cDBYKRjYZho\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "I love uor voice")
+        
+        partyRocks.append(p5)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath ) as? PartyCell {
+            
+            let partyRock = partyRocks[indexPath.row]
+            
+            cell.updateUI(partyRock: partyRock)
+            
+            return cell
+        
+        } else {
+            return UITableViewCell()
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partyRocks.count
     }
 }
 
